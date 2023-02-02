9483c22f34dbd8490bfe9122112e95228ca2d2fd
David Jeong <drumrobot43@gmail.com>
2023-02-02 17:32:01 +0900

    비즈니스 CRUD

--- jex02/src/main/java/org/zerock/service/BoardService.java
+++ jex02/src/main/java/org/zerock/service/BoardService.java
@@ -5,5 +5,14 @@ import java.util.List;
 import org.zerock.domain.BoardVO;
 
 public interface BoardService {
-  public List<BoardVO> getList();
+
+	public void register(BoardVO board);
+
+	public BoardVO get(Long bno);
+
+	public boolean modify(BoardVO board);
+
+	public boolean remove(Long bno);
+
+	public List<BoardVO> getList();
 }
